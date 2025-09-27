4622d3bde55fd9aa276fc7b1b68a705cfb094327
Kenny <106347478+kennyhml@users.noreply.github.com>
2025-09-27 22:55:18 +0200

    refactor: remove info section

--- webviews/AddConnection.svelte
+++ webviews/AddConnection.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import ConnectionList, { type System } from './lib/ConnectionList.svelte';
-	import InfoIcon from './assets/info.svg';
 	import Dropdown from './lib/Dropdown.svelte';
 
 	let height = $state(70);
@@ -34,25 +33,25 @@
 
 <main class="container">
 	<section class="predefined-connections">
-		<h2 class="table-title">Predefined Connections</h2>
+		<h2 class="table-title">Automatically detected Systems</h2>
+		<span>
+			SAP Logon Pad system connections are automatically detected from the
+			installation files or a <a
+				href="https://github.com/kennyhml"
+				target="_blank">manually specified location</a
+			>.
+		</span>
+		<hr />
 		<ConnectionList {systems} {height}></ConnectionList>
-
-		<section class="info-section">
-			<img src={InfoIcon} alt="Information icon" />
-			<div class="text-block">
-				<h2>Predefined Systems can be distributed by your Administrator</h2>
-				<p>
-					These systems are derived from the systems available in the SAP Logon.
-					Please ask your System Administrator to provide you with the necessary
-					file and follow the instructions.
-				</p>
-			</div>
-		</section>
 	</section>
 
 	<section class="custom-connection">
 		<h2 class="table-title">Customize Connection</h2>
-		<p>Configure a connection from scratch or modify a predefined one.</p>
+		<p>
+			Create a new connection from scratch or modify an existing connection from
+			the provided selection.
+		</p>
+		<hr />
 		<Dropdown></Dropdown>
 	</section>
 </main>
@@ -63,32 +62,11 @@
 		flex-direction: row;
 	}
 
-	.info-section {
-		padding-top: 10px;
-		display: flex;
-		align-items: center;
-		gap: 15px;
-	}
-
-	.info-section img {
-		height: 110px;
-		width: 110px;
-	}
-
-	.text-block {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.text-block h2 {
-		font-size: 1.5em;
-		font-weight: bold;
-		margin: 0;
-	}
-
-	.text-block p {
-		font-size: 1.2em;
-		margin: 5px 0 0 0;
+	hr {
+		border: none;
+		height: 1px;
+		background-color: var(--vscode-menu-separatorBackground);
+		margin: 16px 0;
 	}
 
 	.predefined-connections {
