2d34e9155119afb6056f64a99f2c74a79bf99812
Kenny <106347478+kennyhml@users.noreply.github.com>
2025-09-29 18:21:50 +0200

    feat: functional reset button

--- webviews/AddConnection.svelte
+++ webviews/AddConnection.svelte
@@ -1,171 +1,171 @@
 <script lang="ts">
-  import {
-    ConnectionTypes,
-    SecurityLevel,
-    type Connection,
-  } from "types/connection";
-  import ConnectionForm, {
-    type SubmissionResult,
-  } from "./lib/ConnectionForm.svelte";
-  import ConnectionList from "./lib/ConnectionList.svelte";
-  import { onMount } from "svelte";
+	import {
+		ConnectionTypes,
+		SecurityLevel,
+		type Connection,
+	} from 'types/connection';
+	import ConnectionForm, {
+		type SubmissionResult,
+	} from './lib/ConnectionForm.svelte';
+	import ConnectionList from './lib/ConnectionList.svelte';
+	import { onMount } from 'svelte';
 
-  let vscode = acquireVsCodeApi();
+	let vscode = acquireVsCodeApi();
 
-  let connections: Connection[] = $state([]);
-  let formConnectionData: Connection = $state({
-    systemId: "",
-    name: "",
-    displayName: "",
-    description: "",
-    connectionType: ConnectionTypes.CustomApplicationServer,
-    applicationServer: "",
-    instanceNumber: "",
-    sapRouterString: "",
-    sncEnabled: true,
-    ssoEnabled: true,
-    sncName: "",
-    sncLevel: SecurityLevel.Encrypted,
-    keepSynced: true,
-    wasPredefined: false,
-  });
+	let connections: Connection[] = $state([]);
+	let formConnectionData: Connection = $state({
+		systemId: '',
+		name: '',
+		displayName: '',
+		description: '',
+		connectionType: ConnectionTypes.CustomApplicationServer,
+		applicationServer: '',
+		instanceNumber: '',
+		sapRouterString: '',
+		sncEnabled: true,
+		ssoEnabled: true,
+		sncName: '',
+		sncLevel: SecurityLevel.Encrypted,
+		keepSynced: true,
+		wasPredefined: false,
+	});
 
-  async function refreshAvailableConnections() {
-    connections = await getAvailableConnections();
-  }
+	async function refreshAvailableConnections() {
+		connections = await getAvailableConnections();
+	}
 
-  function getAvailableConnections(): Promise<Connection[]> {
-    let interactionId = Math.random().toString(36).substring(2);
-    vscode.postMessage({
-      type: "getConnections",
-      interactionId,
-    });
+	function getAvailableConnections(): Promise<Connection[]> {
+		let interactionId = Math.random().toString(36).substring(2);
+		vscode.postMessage({
+			type: 'getConnections',
+			interactionId,
+		});
 
-    return new Promise((resolve, reject) => {
-      const handleMessage = (event: MessageEvent<any>) => {
-        if (event.data?.interactionId !== interactionId) {
-          return;
-        }
-        window.removeEventListener("message", handleMessage);
-        resolve(event.data?.data as Connection[]);
-      };
-      window.addEventListener("message", handleMessage);
+		return new Promise((resolve, reject) => {
+			const handleMessage = (event: MessageEvent<any>) => {
+				if (event.data?.interactionId !== interactionId) {
+					return;
+				}
+				window.removeEventListener('message', handleMessage);
+				resolve(event.data?.data as Connection[]);
+			};
+			window.addEventListener('message', handleMessage);
 
-      setTimeout(() => {
-        window.removeEventListener("message", handleMessage);
-        reject(new Error("No response received from extension"));
-      }, 5000);
-    });
-  }
+			setTimeout(() => {
+				window.removeEventListener('message', handleMessage);
+				reject(new Error('No response received from extension'));
+			}, 5000);
+		});
+	}
 
-  function onConnectionSubmitted(conn: Connection): Promise<SubmissionResult> {
-    let interactionId = Math.random().toString(36).substring(2);
-    vscode.postMessage({
-      type: "onSubmit",
-      connection: JSON.stringify(conn),
-      interactionId,
-    });
+	function onConnectionSubmitted(conn: Connection): Promise<SubmissionResult> {
+		let interactionId = Math.random().toString(36).substring(2);
+		vscode.postMessage({
+			type: 'onSubmit',
+			connection: JSON.stringify(conn),
+			interactionId,
+		});
 
-    return new Promise((resolve, reject) => {
-      const handleMessage = (event: MessageEvent<any>) => {
-        if (event.data?.interactionId !== interactionId) {
-          return;
-        }
-        window.removeEventListener("message", handleMessage);
-        resolve(event.data as SubmissionResult);
-      };
-      window.addEventListener("message", handleMessage);
+		return new Promise((resolve, reject) => {
+			const handleMessage = (event: MessageEvent<any>) => {
+				if (event.data?.interactionId !== interactionId) {
+					return;
+				}
+				window.removeEventListener('message', handleMessage);
+				resolve(event.data as SubmissionResult);
+			};
+			window.addEventListener('message', handleMessage);
 
-      setTimeout(() => {
-        window.removeEventListener("message", handleMessage);
-        reject(new Error("No response received from extension"));
-      }, 5000);
-    });
-  }
+			setTimeout(() => {
+				window.removeEventListener('message', handleMessage);
+				reject(new Error('No response received from extension'));
+			}, 5000);
+		});
+	}
 
-  function onConnectionTestRequested(
-    conn: Connection
-  ): Promise<SubmissionResult> {
-    console.log(conn);
-    return {} as Promise<SubmissionResult>;
-  }
+	function onConnectionTestRequested(
+		conn: Connection,
+	): Promise<SubmissionResult> {
+		console.log(conn);
+		return {} as Promise<SubmissionResult>;
+	}
 
-  function onSelectionChange(connection: Connection) {
-    formConnectionData = connection;
-  }
+	function onSelectionChange(connection: Connection) {
+		formConnectionData = connection;
+	}
 
-  onMount(async () => {
-    try {
-      connections = await getAvailableConnections();
-    } catch (err) {}
-  });
+	onMount(async () => {
+		try {
+			connections = await getAvailableConnections();
+		} catch (err) {}
+	});
 </script>
 
 <main class="container">
-  <section class="predefined-connections">
-    <h2 class="table-title">Automatically detected Systems</h2>
-    <span>
-      SAP Logon system connections are detected from the installation files or a <a
-        href="https://github.com/kennyhml"
-        target="_blank">manually specified location</a
-      >.
-    </span>
-    <hr />
-    <ConnectionList
-      bind:connections
-      {onSelectionChange}
-      onRefreshRequested={refreshAvailableConnections}
-    ></ConnectionList>
-  </section>
+	<section class="predefined-connections">
+		<h2 class="table-title">Automatically detected Systems</h2>
+		<span>
+			SAP Logon system connections are detected from the installation files or a <a
+				href="https://github.com/kennyhml"
+				target="_blank">manually specified location</a
+			>.
+		</span>
+		<hr />
+		<ConnectionList
+			bind:connections
+			{onSelectionChange}
+			onRefreshRequested={refreshAvailableConnections}
+		></ConnectionList>
+	</section>
 
-  <section class="custom-connection">
-    <h2 class="table-title">Customize Connection</h2>
-    <p>
-      Create a new connection from scratch or modify an existing connection from
-      the provided selection.
-    </p>
-    <hr />
-    <ConnectionForm
-      bind:connectionData={formConnectionData}
-      onSubmit={onConnectionSubmitted}
-      onTest={onConnectionTestRequested}
-    ></ConnectionForm>
-  </section>
+	<section class="custom-connection">
+		<h2 class="table-title">Customize Connection</h2>
+		<p>
+			Create a new connection from scratch or modify an existing connection from
+			the provided selection.
+		</p>
+		<hr />
+		<ConnectionForm
+			bind:connectionData={formConnectionData}
+			onSubmit={onConnectionSubmitted}
+			onTest={onConnectionTestRequested}
+		></ConnectionForm>
+	</section>
 </main>
 
 <style>
-  .container {
-    display: flex;
-    margin: 40px 20px;
-    gap: 50px;
-  }
+	.container {
+		display: flex;
+		margin: 40px 20px;
+		gap: 50px;
+	}
 
-  hr {
-    border: none;
-    height: 1px;
-    background-color: var(--vscode-menu-separatorBackground);
-    margin: 16px 0;
-  }
+	hr {
+		border: none;
+		height: 1px;
+		background-color: var(--vscode-menu-separatorBackground);
+		margin: 16px 0;
+	}
 
-  .predefined-connections {
-    flex: 1.3 1 0;
-  }
+	.predefined-connections {
+		flex: 1.3 1 0;
+	}
 
-  .custom-connection {
-    flex: 1 1 0;
-  }
+	.custom-connection {
+		flex: 1 1 0;
+	}
 
-  .table-title {
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: var(--vscode-editor-foreground);
-    font-family: Arial, sans-serif;
-    text-align: left;
-  }
+	.table-title {
+		font-size: 1.5rem;
+		font-weight: bold;
+		color: var(--vscode-editor-foreground);
+		font-family: Arial, sans-serif;
+		text-align: left;
+	}
 
-  @media (max-width: 1000px) {
-    .container {
-      flex-direction: column;
-    }
-  }
+	@media (max-width: 1000px) {
+		.container {
+			flex-direction: column;
+		}
+	}
 </style>
--- webviews/lib/ConnectionList.svelte
+++ webviews/lib/ConnectionList.svelte
@@ -1,226 +1,235 @@
 <script lang="ts">
-  import type { Connection } from "types/connection";
-  import TextInput from "./TextInput.svelte";
-  import SecureIcon from "../assets/secure.svg";
-  import InsecureIcon from "../assets/insecure.svg";
-  import SSOEnabledIcon from "../assets/ssoEnabled.svg";
-  import SSODisabledIcon from "../assets/ssoDisabled.svg";
-  import RefreshIcon from "../assets/refresh.svg";
+	import type { Connection } from 'types/connection';
+	import TextInput from './TextInput.svelte';
+	import SecureIcon from '../assets/secure.svg';
+	import InsecureIcon from '../assets/insecure.svg';
+	import SSOEnabledIcon from '../assets/ssoEnabled.svg';
+	import SSODisabledIcon from '../assets/ssoDisabled.svg';
 
-  let {
-    connections = $bindable(),
-    onSelectionChange,
-    onRefreshRequested,
-  }: {
-    connections: Connection[];
-    onSelectionChange: (connection: Connection) => void;
-    onRefreshRequested: () => void;
-  } = $props();
+	let {
+		connections = $bindable(),
+		onSelectionChange,
+		onRefreshRequested,
+	}: {
+		connections: Connection[];
+		onSelectionChange: (connection: Connection) => void;
+		onRefreshRequested: () => void;
+	} = $props();
 
-  type UniqueConnection = Connection & { id: number };
+	type UniqueConnection = Connection & { id: number };
 
-  let mappedConnections = $derived(
-    connections
-      .toSorted((a, b) => a.systemId.localeCompare(b.systemId))
-      .map((v, i) => ({ ...v, id: i }))
-  );
+	let mappedConnections = $derived(
+		connections
+			.toSorted((a, b) => a.systemId.localeCompare(b.systemId))
+			.map((v, i) => ({ ...v, id: i })),
+	);
 
-  let searchFilter: string = $state("");
-  let matchingEntries = $derived(
-    getMatchingConnections(mappedConnections, searchFilter)
-  );
+	let searchFilter: string = $state('');
+	let matchingEntries = $derived(
+		getMatchingConnections(mappedConnections, searchFilter),
+	);
 
-  let selectedId: number = $state(-1);
+	let selectedId: number = $state(-1);
 
-  function getMatchingConnections(
-    conns: UniqueConnection[],
-    filter: string
-  ): UniqueConnection[] {
-    if (!filter) {
-      return conns;
-    }
-    let lowerFilter = filter.toLowerCase();
-    return conns.filter(
-      (conn) =>
-        conn.systemId.toLowerCase().includes(lowerFilter) ||
-        conn.name.toLowerCase().includes(lowerFilter)
-    );
-  }
+	function getMatchingConnections(
+		conns: UniqueConnection[],
+		filter: string,
+	): UniqueConnection[] {
+		if (!filter) {
+			return conns;
+		}
+		let lowerFilter = filter.toLowerCase();
+		return conns.filter(
+			(conn) =>
+				conn.systemId.toLowerCase().includes(lowerFilter) ||
+				conn.name.toLowerCase().includes(lowerFilter),
+		);
+	}
 
-  function onSelected(connection: UniqueConnection) {
-    if (selectedId !== connection.id) {
-      selectedId = connection.id;
-      const { id, ...conn } = connection;
-      onSelectionChange(conn);
-    }
-  }
+	function onSelected(connection: UniqueConnection) {
+		if (selectedId !== connection.id) {
+			selectedId = connection.id;
+			const { id, ...conn } = connection;
+			onSelectionChange(conn);
+		}
+	}
 
-  function noneMatchFilter(): boolean {
-    return mappedConnections.length !== 0 && matchingEntries.length === 0;
-  }
+	function noneMatchFilter(): boolean {
+		return mappedConnections.length !== 0 && matchingEntries.length === 0;
+	}
 </script>
 
 <div class="content">
-  <div class="header">
-    <TextInput
-      bind:value={searchFilter}
-      placeholder="Search"
-      style="width: 60%; margin-bottom: 5px; border-radius: 2px"
-    />
-    <img src={RefreshIcon} alt="Refresh" onclick={onRefreshRequested} />
-  </div>
+	<div class="header">
+		<TextInput
+			bind:value={searchFilter}
+			placeholder="Search"
+			style="width: 60%; margin-bottom: 5px; border-radius: 2px"
+		/>
+		<button type="button" onclick={onRefreshRequested} aria-label="Refresh">
+		</button>
+	</div>
 
-  <div class="table-container">
-    <table>
-      <thead>
-        <tr>
-          <th>SID</th>
-          <th>Name</th>
-          <th>Description</th>
-          <th>SNC</th>
-          <th>SSO</th>
-          <th>Router</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each matchingEntries as conn (conn.id)}
-          <tr
-            onclick={() => onSelected(conn)}
-            class:highlighted={conn.id === selectedId}
-          >
-            <td>{conn.systemId}</td>
-            <td>{conn.name}</td>
-            <td>{conn.description}</td>
-            {#if conn.sncEnabled}
-              <td class="icon"><img src={SecureIcon} alt="Secure" /></td>
-            {:else}
-              <td class="icon"><img src={InsecureIcon} alt="Insecure" /></td>
-            {/if}
-            {#if conn.ssoEnabled}
-              <td class="icon"><img src={SSOEnabledIcon} alt="Secure" /></td>
-            {:else}
-              <td class="icon"><img src={SSODisabledIcon} alt="Insecure" /></td>
-            {/if}
-            <td>{conn.sapRouterString}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+	<div class="table-container">
+		<table>
+			<thead>
+				<tr>
+					<th>SID</th>
+					<th>Name</th>
+					<th>Description</th>
+					<th>SNC</th>
+					<th>SSO</th>
+					<th>Router</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each matchingEntries as conn (conn.id)}
+					<tr
+						onclick={() => onSelected(conn)}
+						class:highlighted={conn.id === selectedId}
+					>
+						<td>{conn.systemId}</td>
+						<td>{conn.name}</td>
+						<td>{conn.description}</td>
+						{#if conn.sncEnabled}
+							<td class="icon"><img src={SecureIcon} alt="Secure" /></td>
+						{:else}
+							<td class="icon"><img src={InsecureIcon} alt="Insecure" /></td>
+						{/if}
+						{#if conn.ssoEnabled}
+							<td class="icon"><img src={SSOEnabledIcon} alt="Secure" /></td>
+						{:else}
+							<td class="icon"><img src={SSODisabledIcon} alt="Insecure" /></td>
+						{/if}
+						<td>{conn.sapRouterString}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 
-    {#if connections.length === 0}
-      <p class="not-found">
-        No connections found. Follow <a href="https://github.com/kennyhml"
-          >the instructions</a
-        > to add a connection provider.
-      </p>
-    {:else if noneMatchFilter()}
-      <p class="not-found">
-        No connection matches this filter - please check again.
-      </p>
-    {/if}
-  </div>
+		{#if connections.length === 0}
+			<p class="not-found">
+				No connections found. Follow <a href="https://github.com/kennyhml"
+					>the instructions</a
+				> to add a connection provider.
+			</p>
+		{:else if noneMatchFilter()}
+			<p class="not-found">
+				No connection matches this filter - please check again.
+			</p>
+		{/if}
+	</div>
 </div>
 
 <style>
-  .header {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+	.header {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 
-    gap: 10px;
-  }
+		gap: 10px;
+	}
 
-  .header img {
-    width: 18px;
-    height: 18px;
-    margin-right: 10px;
-  }
+	.table-container {
+		overflow-y: auto;
+		max-height: 70vh;
+	}
 
-  .table-container {
-    overflow-y: auto;
-    max-height: 70vh;
-  }
+	.icon {
+		text-align: center;
+		vertical-align: bottom;
+	}
 
-  .icon {
-    text-align: center;
-    vertical-align: bottom;
-  }
+	.icon img {
+		margin-top: 2px;
+		margin-bottom: -3px;
+		width: 18px;
+		height: 18px;
+	}
 
-  .icon img {
-    margin-top: 2px;
-    margin-bottom: -3px;
-    width: 18px;
-    height: 18px;
-  }
+	.not-found {
+		width: 100%;
+		text-align: center;
+		font-weight: bold;
+	}
 
-  .not-found {
-    width: 100%;
-    text-align: center;
-    font-weight: bold;
-  }
+	thead {
+		position: sticky;
+		top: 0;
+		z-index: 1;
+	}
 
-  thead {
-    position: sticky;
-    top: 0;
-    z-index: 1;
-  }
+	th,
+	td {
+		padding: 4px 4px;
+		text-align: left;
+		border-left: 1px solid var(--vscode-sideBar-border);
+		border-bottom: 1px solid var(--vscode-sideBar-border);
+		white-space: nowrap;
 
-  th,
-  td {
-    padding: 4px 4px;
-    text-align: left;
-    border-left: 1px solid var(--vscode-sideBar-border);
-    border-bottom: 1px solid var(--vscode-sideBar-border);
-    white-space: nowrap;
+		color: var(--vscode-editor-foreground);
+	}
 
-    color: var(--vscode-editor-foreground);
-  }
+	td:last-child {
+		border-right: 1px solid var(--vscode-sideBar-border);
+	}
 
-  td:last-child {
-    border-right: 1px solid var(--vscode-sideBar-border);
-  }
+	td:first-child {
+		border-left: 1px solid var(--vscode-sideBar-border);
+	}
 
-  td:first-child {
-    border-left: 1px solid var(--vscode-sideBar-border);
-  }
+	.highlighted td {
+		border-top: 1px solid var(--vscode-inputOption-activeBorder);
+		border-bottom: 1px solid var(--vscode-inputOption-activeBorder);
+	}
 
-  .highlighted td {
-    border-top: 1px solid var(--vscode-inputOption-activeBorder);
-    border-bottom: 1px solid var(--vscode-inputOption-activeBorder);
-  }
+	.highlighted td:first-child {
+		border-left: 1px solid var(--vscode-inputOption-activeBorder);
+	}
 
-  .highlighted td:first-child {
-    border-left: 1px solid var(--vscode-inputOption-activeBorder);
-  }
+	.highlighted td:last-child {
+		border-right: 1px solid var(--vscode-inputOption-activeBorder);
+	}
 
-  .highlighted td:last-child {
-    border-right: 1px solid var(--vscode-inputOption-activeBorder);
-  }
+	table {
+		width: 100%;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		font-family: Arial, sans-serif;
+		table-layout: auto;
+		border-spacing: 0;
+		border-collapse: separate;
+	}
 
-  table {
-    width: 100%;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    font-family: Arial, sans-serif;
-    table-layout: auto;
-    border-spacing: 0;
-    border-collapse: separate;
-  }
+	tr:nth-child(odd) {
+		background-color: #2d2d2d;
+	}
+	tr:nth-child(even) {
+		background-color: #3c3c3c;
+	}
 
-  tr:nth-child(odd) {
-    background-color: #2d2d2d;
-  }
-  tr:nth-child(even) {
-    background-color: #3c3c3c;
-  }
+	tr:hover {
+		background-color: var(--vscode-sideBar-background);
+	}
 
-  tr:hover {
-    background-color: var(--vscode-sideBar-background);
-  }
+	th {
+		font-weight: bold;
+		font-size: 1.15em;
+		color: var(--vscode-list-focusForeground);
+		background-color: var(--vscode-activityBar-background);
+	}
 
-  th {
-    font-weight: bold;
-    font-size: 1.15em;
-    color: var(--vscode-list-focusForeground);
-    background-color: var(--vscode-activityBar-background);
-  }
+	button {
+		background-image: url(../assets/refresh.svg);
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: contain;
+		background-color: inherit;
+		border: none;
+		width: 20px;
+		height: 20px;
+	}
+
+	button:active {
+		transform: scale(0.85);
+	}
 </style>
