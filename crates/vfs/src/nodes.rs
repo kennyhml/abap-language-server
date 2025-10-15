e1a7b82d333cc354d5c904f350a0ba1920cde040
Kenny <106347478+kennyhml@users.noreply.github.com>
2025-10-15 23:53:38 +0200

    feat: add internal & external node typing

--- crates/vfs/src/nodes.rs
+++ crates/vfs/src/nodes.rs
@@ -1,40 +1,123 @@
 use adt_query::models::vfs::{Facet, RepositoryObject};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-pub enum NodeGroup {
-    LocalObjects,
-
-    SystemLibrary,
-
-    Favorites,
+pub trait NodeDetail {
+    type Facet: Default;
+    type Group: Default;
 }
 
-impl NodeGroup {
-    pub fn display_name(&self) -> &'static str {
+#[derive(Default)]
+pub struct External {}
+pub struct Internal {}
+
+impl NodeDetail for External {
+    type Facet = External;
+    type Group = External;
+}
+
+impl NodeDetail for Internal {
+    type Facet = FacetInternal;
+    type Group = GroupInternal;
+}
+
+/// Represents any node in the filesystem.
+///
+/// The implications, purpose and expansion details depends on the variant.
+#[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
+#[serde(tag = "kind", rename_all = "camelCase")]
+pub enum FilesystemNode<T: NodeDetail> {
+    /// A, possibly custom, grouping for organizational purposes.
+    Group(GroupNode<T::Group>),
+
+    /// Similar to Groups except they work on properties of objects and are the
+    /// fundamental means of filtering the repository with ADT.
+    Facet(FacetNode<T::Facet>),
+
+    /// An actual object in the repository that provides data such as source
+    /// code, definitions or table contents and cant be expanded.
+    ///
+    /// Eclipse allows you to expand these nodes into the individual things defined
+    /// in them, but that is not possible with vscode folders.
+    RepositoryObject(RepositoryObjectNode),
+}
+
+impl FilesystemNode<Internal> {
+    pub fn children(&self) -> Option<&Vec<FilesystemNode<Internal>>> {
         match self {
-            Self::LocalObjects => "Local Objects",
-            Self::SystemLibrary => "System Library",
-            Self::Favorites => "Favorites",
+            FilesystemNode::Facet(f) => f.detail.children.as_ref(),
+            FilesystemNode::Group(g) => g.detail.children.as_ref(),
+            FilesystemNode::RepositoryObject(..) => None,
         }
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct GroupNode {
-    name: String,
-
-    id: NodeGroup,
-
-    /// The children of the group node, i.e the nodes it groups together.
-    children: Option<Vec<FilesystemNode>>,
+impl PartialEq<FilesystemNode<Internal>> for FilesystemNode<External> {
+    fn eq(&self, other: &FilesystemNode<Internal>) -> bool {
+        match (self, other) {
+            (FilesystemNode::Group(g1), FilesystemNode::Group(g2)) => g1 == g2,
+            (FilesystemNode::Facet(f1), FilesystemNode::Facet(f2)) => f1 == f2,
+            (FilesystemNode::RepositoryObject(r1), FilesystemNode::RepositoryObject(r2)) => {
+                r1 == r2
+            }
+            _ => false,
+        }
+    }
 }
 
+impl PartialEq<FilesystemNode<External>> for FilesystemNode<Internal> {
+    fn eq(&self, other: &FilesystemNode<External>) -> bool {
+        other == self
+    }
+}
+
+/// Custom categorization of items into groups for organizational purposes.
+///
+/// How these nodes expand depends on the underlying group.
 #[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct FacetNode {
+pub struct GroupNode<T> {
+    /// The display name of the group in the filesystem.
+    name: String,
+
+    /// The technical definition of the group.
+    group: Group,
+
+    #[serde(skip)]
+    detail: T,
+}
+
+impl PartialEq<GroupNode<External>> for GroupNode<GroupInternal> {
+    fn eq(&self, other: &GroupNode<External>) -> bool {
+        self.name == other.name && self.group == other.group
+    }
+}
+
+impl PartialEq<GroupNode<GroupInternal>> for GroupNode<External> {
+    fn eq(&self, other: &GroupNode<GroupInternal>) -> bool {
+        other == self
+    }
+}
+
+#[derive(Default)]
+pub struct GroupInternal {
+    /// The children of the group node, i.e the nodes it groups together.
+    ///
+    /// Not included in serialization as we never send more than a layer of children
+    /// per request from the client (one expand only).
+    children: Option<Vec<FilesystemNode<Internal>>>,
+}
+
+/// Represents a facet node in the fileystem.
+///
+/// A facet referes to a specific attribute of an object. For example, an `Owner`
+/// facet with the value `DEVELOPER` groups all objects created by `DEVELOPER`.
+///
+/// The main difference between a [Facet] compared to a [Group] is that they are
+/// part of the ADT representation of the VFS rather than groupings we can may
+/// create ourselves.
+#[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FacetNode<T> {
     /// The kind of facet, see [Facet]
     facet: Facet,
 
@@ -48,14 +131,51 @@ struct FacetNode {
     /// In the case of a `TYPE` facet, this could, for example, be `CLAS` or `PROG`..
     technical_name: String,
 
-    package: Option<String>,
+    /// If the facet is not itself a `PACKAGE` facet, then the parent package in the hierarchy.
+    ///
+    /// This is needed to uniquely identify the position of the facet in the filesystem, as
+    /// packages generally expand into the same structure.
+    pub package: Option<String>,
 
-    children: Option<Vec<FilesystemNode>>,
+    #[serde(skip)]
+    detail: T,
 }
 
+impl PartialEq<FacetNode<FacetInternal>> for FacetNode<External> {
+    fn eq(&self, other: &FacetNode<FacetInternal>) -> bool {
+        self.facet == other.facet
+            && self.name == other.name
+            && self.technical_name == other.technical_name
+            && self.package == other.package
+    }
+}
+
+impl PartialEq<FacetNode<External>> for FacetNode<FacetInternal> {
+    fn eq(&self, other: &FacetNode<External>) -> bool {
+        other == self
+    }
+}
+
+#[derive(Default)]
+pub struct FacetInternal {
+    /// The children of the group node, i.e the nodes it groups together.
+    ///
+    /// Not included in serialization as we never send more than a layer of children
+    /// per request from the client (one expand only).
+    pub children: Option<Vec<FilesystemNode<Internal>>>,
+
+    /// Whether this node has child facets of the same kind. This is typically the case with
+    /// packages that contain sub-packages. Since packages and other facets need to be fetched
+    /// individually, this helps optimize that additional fetch away if we know it is not needed.
+    pub has_children_of_same_facet: bool,
+}
+
+/// Represents a repository object node in the filesystem.
+///
+/// These nodes are generally leaf nodes as repository objects do not have successors.
 #[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct RepositoryObjectNode {
+pub struct RepositoryObjectNode {
     /// The name of the repository object
     name: String,
 
@@ -63,39 +183,20 @@ struct RepositoryObjectNode {
     object_kind: RepositoryObject,
 }
 
+/// Possible categorization options for [GroupNode]
 #[derive(Debug, Eq, PartialEq, Clone, Serialize, Deserialize)]
-#[serde(tag = "kind", rename_all = "camelCase")]
-pub enum FilesystemNode {
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum Group {
+    /// The system root
     System,
-    Group(GroupNode),
-    Facet(FacetNode),
-    RepositoryObject(RepositoryObjectNode),
-}
 
-impl FilesystemNode {
-    pub fn group(group: NodeGroup) -> Self {
-        FilesystemNode::Group(GroupNode {
-            name: group.display_name().into(),
-            id: group,
-            children: None,
-        })
-    }
-}
+    /// Categorizes nodes which are not part of the system library. This __does not__
+    /// exclude local objects from other developers in the same system.
+    LocalObjects,
 
-/// Parameters for **`filesystem/expand`**
-///
-/// and an editor context for the requested system.
-#[derive(Debug, Eq, PartialEq, Clone, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ExpandParams {
-    /// The node to expand, should have no children assigned.
-    pub node: FilesystemNode,
-}
+    /// Categorizes nodes which are part of the public system library
+    SystemLibrary,
 
-/// Response of **`filesystem/expand`**
-#[derive(Debug, Eq, PartialEq, Clone, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ExpandResult {
-    /// The child nodes of the node to be expanded.
-    pub children: Vec<FilesystemNode>,
+    /// Categorizes nodes which the user has explicitly favorited.
+    Favorites,
 }
--- crates/vfs/src/tree.rs
+++ crates/vfs/src/tree.rs
@@ -1,8 +1,55 @@
+use std::collections::HashMap;
+
+use crate::nodes::{External, FilesystemNode, Internal};
+
 /// Represents a filesystem as tree of Nodes
-pub struct VirtualFileTree {}
+pub struct VirtualFileTree {
+    root: FilesystemNode<Internal>,
+
+    /// An additional index mapping package names to their nodes
+    by_package: HashMap<String, FilesystemNode<Internal>>,
+}
 
 impl VirtualFileTree {
-    pub fn lookup() {
+    // Expands the given node
+    pub fn expand(
+        &self,
+        node: &FilesystemNode<Internal>,
+    ) -> Option<&Vec<FilesystemNode<Internal>>> {
         todo!()
+    }
+
+    /// Searches for a given node in the filetree and returns a reference to it.
+    ///
+    /// While the input is a node just like the output, the input is typically a deserialized
+    /// variant which doesnt actually exist in the file tree and is more of a copy of the nodes data.
+    pub fn lookup(&self, node: &FilesystemNode<External>) -> Option<&FilesystemNode<Internal>> {
+        fn dfs<'a>(
+            curr: &'a FilesystemNode<Internal>,
+            target: &FilesystemNode<External>,
+        ) -> Option<&'a FilesystemNode<Internal>> {
+            if curr == target {
+                return Some(curr);
+            }
+            for child in curr.children()?.iter() {
+                if let Some(found) = dfs(child, target) {
+                    return Some(found);
+                }
+            }
+            None
+        }
+
+        // If we know the package this node is part of we can possibly optimize lookup
+        let package = match node {
+            FilesystemNode::Facet(f) => f.package.as_ref(),
+            _ => None,
+        };
+
+        if let Some(package) = package {
+            if let Some(pkg_node) = self.by_package.get(package) {
+                return dfs(pkg_node, node);
+            }
+        }
+        dfs(&self.root, node)
     }
 }
